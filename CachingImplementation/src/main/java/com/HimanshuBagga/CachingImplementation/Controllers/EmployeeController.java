63af8954470707c473192eed99874fec13520bfd
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-02-06 01:55:48 +0530

    AddingControllerForAPIgetEmployeebyId

--- CachingImplementation/src/main/java/com/HimanshuBagga/CachingImplementation/Controllers/EmployeeController.java
+++ CachingImplementation/src/main/java/com/HimanshuBagga/CachingImplementation/Controllers/EmployeeController.java
@@ -1,4 +1,29 @@
 package com.HimanshuBagga.CachingImplementation.Controllers;
 
+import com.HimanshuBagga.CachingImplementation.dto.EmployeeDto;
+import com.HimanshuBagga.CachingImplementation.services.EmployeeService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/Employee/v1")
+@FieldDefaults(makeFinal = true , level = AccessLevel.PRIVATE)
 public class EmployeeController {
+
+    EmployeeService employeeService;
+
+
+    @GetMapping("/{employeeId}")
+    public ResponseEntity<EmployeeDto> getEmployeebyId(@PathVariable Long employeeId){
+        EmployeeDto employeeDto = employeeService.getEmployeebyId(employeeId);
+        return ResponseEntity.ok(employeeDto);
+    }
+
 }
